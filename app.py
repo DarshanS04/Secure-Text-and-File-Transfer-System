1bf82794adc586a8417a5b7efb4f1407c347f3e1
DarshanGitHub123 <vonemaker75@gmail.com>
2026-03-30 21:49:11 +0530

    add 0.0.0.0 to app.py

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def load_user(user_id):
     return User(user_doc) if user_doc else None
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
